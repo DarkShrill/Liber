6dc87627d33585727b57c46ea724cd4fcdc76e3f
Marica <marica.quadrini@studenti.unicam.it>
2020-02-12 00:36:26 +0100

    - Aggiornate CORS Policy delle API -> Data: 12/02/2020 @marica

--- Liber Server/book/delete.php
+++ Liber Server/book/delete.php
@@ -1,9 +1,14 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header("Access-Control-Allow-Origin: *");   
+    header("Access-Control-Allow-Headers: *");
+    http_response_code(200);
+    exit;
+}
+
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: DELETE");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 include_once '../config/Database.php';
 include_once '../config/ActivityController.php';
--- Liber Server/login/login.php
+++ Liber Server/login/login.php
@@ -1,9 +1,14 @@
 <?php 
-    header("Access-Control-Allow-Origin: *");
-    header("Content-Type: application/json; charset=UTF-8");
-    header("Access-Control-Allow-Methods: POST");
-    header("Access-Control-Max-Age: 3600");
-    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header("Access-Control-Allow-Origin: *");   
+    header("Access-Control-Allow-Headers: *");
+    http_response_code(200);
+    exit;
+}
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Content-Type: application/json; charset=UTF-8");
     
     include_once '../config/Database.php';
     include_once '../config/ActivityController.php';
